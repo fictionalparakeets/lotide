9b6d531222e0afdf862261da89f9f9b92809c5ef
fictionalparakeets <banksmatthew32@gmail.com>
2021-06-23 20:57:22 +0000

    a function that returns an object with all letters in a string and their corresponding indexes

---
+++ letterPositions.js
@@ -0,0 +1,26 @@
+const letterPositions = function(inputString) {
+  let letterObject = {};
+  let indexCount = 0;
+  for (let letter of inputString) {
+    if (letterObject[letter]) {
+      letterObject[letter].push(inputString.indexOf(letter, indexCount));
+    } else {
+      letterObject[letter] = [inputString.indexOf(letter)];
+    }
+    indexCount += 1;
+  }
+  return printObject(letterObject);
+};
+
+const printObject = function(objectInput) {
+  let message = '';
+  for (let entry in objectInput) {
+    let key = entry;
+    let value = objectInput[entry].toString();
+    message += `Key: ${key}, Value: ${value}\n`;
+  }
+  return message;
+};
+
+// Tests:
+console.log(letterPositions("lighthouse in the house"));
