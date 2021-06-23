2f961287a1802229b0bb9dd726ef0c31d2544ae9
fictionalparakeets <banksmatthew32@gmail.com>
2021-06-23 01:14:33 +0000

    minor linting fixes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`🏆🏆🏆 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -10,9 +10,9 @@ const eqArrays = function(arr1, arr2) {
   const lengthMatch = arr1.length === arr2.length;
   const stringHackMatch = JSON.stringify(arr1) === JSON.stringify(arr2);
   return (lengthMatch && stringHackMatch);
-}
+};
 
-assertEqual([1, 2, 3], [1, 2, 3])
-assertEqual([1, 2, 3], [3, 2, 1])
-assertEqual(["1", "2", "3"], ["1", "2", "3"])
-assertEqual(["1", "2", "3"], ["1", "2", 3])
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
--- eqArrays.js
+++ eqArrays.js
@@ -10,7 +10,7 @@ const eqArrays = function(arr1, arr2) {
   const lengthMatch = arr1.length === arr2.length;
   const stringHackMatch = JSON.stringify(arr1) === JSON.stringify(arr2);
   return (lengthMatch && stringHackMatch);
-}
+};
 
 // Tests:
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
