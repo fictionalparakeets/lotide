660de9c8873e55b4e9e633719d2da1a31033a0e7
fictionalparakeets <banksmatthew32@gmail.com>
2021-06-23 01:33:10 +0000

    add tool for removing items from an array and then checking for equality

---
+++ without.js
@@ -0,0 +1,32 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🏆🏆🏆 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  const lengthMatch = arr1.length === arr2.length;
+  const stringHackMatch = JSON.stringify(arr1) === JSON.stringify(arr2);
+  return (lengthMatch && stringHackMatch);
+};
+
+const without = function(actual, expected) {
+  let arrayWithout = [];
+  for (let item of actual) {
+    if (!expected.includes(item)) {
+      arrayWithout.push(item);
+    }
+  }
+  return arrayWithout;
+}
+
+// Tests:
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
